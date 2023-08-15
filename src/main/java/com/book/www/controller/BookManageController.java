6a1cbbd4e258675fa0d3e75af1f2585af17a91a8
sjxt <2195499047@qq.com>
2023-08-15 09:51:47 +0800

    增加功能

--- src/main/java/com/book/www/controller/BookManageController.java
+++ src/main/java/com/book/www/controller/BookManageController.java
@@ -1,18 +1,36 @@
 package com.book.www.controller;
 
+import com.book.www.entity.Bookmanage;
 import com.book.www.service.BookmanageService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 
 @Controller
+@Slf4j
 public class BookManageController {
-  @Resource
-  BookmanageService bookmanageService;
+    @Resource
+    BookmanageService bookmanageService;
 
-  @GetMapping("/")
-  public String index() {
-    return "index";
-  }
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/add.html")
+    public String fromAdd() {
+        return "add";
+    }
+
+    @RequestMapping("/add")
+    public String add(Bookmanage bookmanage) {
+        if (bookmanageService.save(bookmanage)) {
+            return "redirect:/";
+        } else {
+            return "redirect:/add";
+        }
+    }
 }
